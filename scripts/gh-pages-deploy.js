8f21f573803842d68a86b5716f455e326a1359ab
Luis Melendez <luis.melendez@busolinea.com>
2020-05-06 21:36:47 -0500

    edit script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -13,8 +13,6 @@ const fs = require("fs");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
     console.log(e.message);
