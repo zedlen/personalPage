ea6361a696a898795287920ce65109144e568ceb
Luis Melendez <luis.melendez@busolinea.com>
2020-05-06 21:46:44 -0500

    fix missing var

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const Dotenv = require("dotenv-webpack");
+const debug = process.env.NODE_ENV !== "production";
 module.exports = { 
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         config.plugins.push(new Dotenv({ silent: true }));
