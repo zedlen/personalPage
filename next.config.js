9c1fb3d80333b9c7c9a430745596f9fc9d6f7002
Luis Melendez <luis.zdn@gmail.com>
2020-05-07 00:53:20 -0500

    test envs

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
         config.plugins.push(new Dotenv({ silent: true }));
         return config;
     },
+    env: {
+        BLOGGER_API_KEY: process.env.BLOGGER_API_KEY,
+        BLOG_ID: process.env.BLOG_ID
+    }
 }
