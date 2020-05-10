706f04a59ed96455adfe2ce4f8bbbb606c0fbda4
Luis Melendez <luis.zdn@gmail.com>
2020-05-09 21:33:27 -0500

    add serveless functions

---
+++ api/mail/index.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer'); // email sender function 
+module.exports = async (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        }
+    })
+    var mailOptions = {
+        from: 'Zedlen Page',
+        to: 'luis.melencez@zedlen.com',
+        subject: 'Zedlen Contact',
+        text: 'Contenido del email'
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error){
+            console.log(error);
+            res.send(500, err.message);
+        } else {
+            console.log("Email sent");
+            res.status(200).jsonp(req.body);
+        }
+    });     
+}
